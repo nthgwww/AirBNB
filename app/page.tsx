4969d6732df0a1443b86b3a0deda1da693e97247
ngocthong <6051071118@st.utc2.edu.vn>
2025-02-28 11:22:27 +0700

    logo-navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-// import Image from "next/image";
-
 export default function Home() {
   return (
-    <div>
-      Home
+    <div className="text-rose-500 text-2xl">
+      Hello AirBNB!
     </div>
   );
 }
